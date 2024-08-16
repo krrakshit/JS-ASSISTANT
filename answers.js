d0594c14fa0af055c1525f53310939b7167bfb6d
krrakshit <rkrkr365@gmail.com>
2024-08-16 15:56:23 +0530

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,14 @@
+let intro = ["i am your robot", "hi i am a robo", "my name is chitti hah ha ha ",
+ "i am your personal assistent", "i am machine"];
+
+ let help =["how may i assistant you", "how i can halp you", "what i can do for you?"];
+
+ let names = ["you are my friend", "your name is robo", "mr. robo"];
+
+ let movie = ["titanic", "final destination", "aircraft"];
+
+ let thanks = ["most welcome", "as you wish", "mention not"];
+
+ let work = ["full stack developer", "software engneer"];
+
+ let close = ["okay by by", "as you wish", "see you soon...."];
